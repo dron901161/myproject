9e57cde74c9c9fe9ce04f144c9744e179cf44355
Dron <andreimezhiritsky@yandex.ru>
2022-04-19 14:10:19 +0300

    Добавлены переменные, alert, console.log

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = 1;
+let screens = 2;
+let screenPrice = "Любой текст";
+let rollback = "Простая переменная";
+const fullPrice = 3;
+const adaptive = 4;
+
+alert("Hello World!" + " " + screenPrice);
+console.log(title + " " + rollback + " " + "Hello World!");
