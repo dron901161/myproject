9dddf1ee7bfd84af7beb7ad565c9c6421523d445
Dron <andreimezhiritsky@yandex.ru>
2022-05-02 20:23:18 +0300

    Изменение переменных с let на const

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let title = prompt("Как называется ваш проект?");
-let screens = prompt("Какие типы экранов нужно разработать?", "пример: Простые, Сложные, Интерактивные");
-let screenPrice = +prompt("Сколько будет стоить данная работа?", "пример: 12000");
-let adaptive = confirm("Нужен ли адаптив на сайте?");
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = +prompt("Сколько это будет стоить?");
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = +prompt("Сколько это будет стоить?");
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let rollback = 15; // Откат посреднику
-let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+const title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?", "пример: Простые, Сложные, Интерактивные");
+const screenPrice = +prompt("Сколько будет стоить данная работа?", "пример: 12000");
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?");
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const rollback = 15; // Откат посреднику
+const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
 
 let allServicePrices;
 
